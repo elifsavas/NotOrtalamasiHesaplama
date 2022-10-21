56167a0a955cb1f8527c1d324ef3646e71926a15
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-21 22:12:28 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner ;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    // Degiskenleri olustur
+
+        int mat, fzk, kim, turk, trh, mzk ;
+        Scanner inp = new Scanner(System.in) ;
+        //Kullanicidan degerleri al
+
+        System.out.print("Matematik Notunuz :");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Notunuz:");
+        fzk = inp.nextInt();
+
+        System.out.print("Kimya Notunuz:");
+        kim = inp.nextInt() ;
+
+        System.out.print("Türkçe Notunuz:");
+        turk = inp.nextInt();
+
+        System.out.print("Tarih Notunuz:");
+        trh = inp.nextInt();
+
+        System.out.print("Müzik Notunuz:");
+        mzk = inp.nextInt();
+
+        int toplam = (mat+ fzk + kim + turk + trh + mzk) ;
+        double sonuc = toplam/6 ;
+        System.out.println("Ortalama Notunuz:" + sonuc);
+
+        String result;
+        result = (sonuc>60) ? "Gectiniz" : "Kaldınız" ;
+        System.out.println(result);
+
     }
 }
